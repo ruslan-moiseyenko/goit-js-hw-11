9b620f7aeac37a2ae6d195c7fcb63376eaf4bf46
Ruslan Moiseyenko <ruscom5@gmail.com>
2022-02-07 22:32:31 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './css/styles.css';
+import "normalize.css";
+import fetchCountries from './fetchCountries.js';
+const debounce = require('lodash.debounce');
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import "notiflix/dist/notiflix-3.2.4.min.css";
+
