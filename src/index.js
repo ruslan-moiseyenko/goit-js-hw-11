b42ac22d85bd482482b8c85239b324bcd2997d89
Ruslan Moiseyenko <ruscom5@gmail.com>
2022-02-07 23:39:42 +0200

    added search form

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import './css/styles.css';
 import "normalize.css";
 import fetchCountries from './fetchCountries.js';
 const debounce = require('lodash.debounce');
+const axios = require('axios').default;
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import "notiflix/dist/notiflix-3.2.4.min.css";
 
+const options = {
+  method: 'GET',
+  headers: { 'content-type': 'application/x-www-form-urlencoded' },
+  data: qs.stringify(data),
+  url,
+};
